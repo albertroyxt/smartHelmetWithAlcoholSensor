0106b55ffef595bae73b3ce32a8d4be05e1dbe90
Albert Roy <38367773+albertroyxt@users.noreply.github.com>
2018-10-31 23:00:54 +0530

    Add files via upload

---
+++ rxCompiled.ino
@@ -0,0 +1,80 @@
+#include <VirtualWire.h>
+#include <VirtualWire_Config.h>
+#include <LiquidCrystal.h>
+const int LCDVSS=2;
+const int LCDVCC=3;
+const int rs=4;
+const int rw=5;
+const int en=6;
+const int d4=7;
+const int d5=8;
+const int d6=9;
+const int d7=10;
+
+const int relayout1=A0;
+const int relayout2=A1;
+
+const int RX_VCC = 11;
+const int RX_PIN = 12;// Tell Arduino on which pin you would like to receive data NOTE should be a PWM Pin
+const int RX_GND = 13;
+const int RX_ID = 3;// Recever ID address 
+int TX_ID;
+struct roverRemoteData //Data Structure 
+{
+int    TX_ID;      // Initialize a storage place for the incoming TX ID  
+int    Sensor1Data;// Initialize a storage place for the first integar that you wish to Receive 
+int    Sensor2Data;// Initialize a storage place for the Second integar that you wish to Receive
+int    Sensor3Data;// Initialize a storage place for the  Third integar that you wish to Receive
+int    Sensor4Data;// Initialize a storage place for the Forth integar that you wish to Receive
+
+};
+
+void setup() {
+              pinMode(rw, OUTPUT);
+              pinMode(LCDVCC, OUTPUT);
+              pinMode(LCDVSS, OUTPUT);
+
+              digitalWrite(LCDVCC, HIGH);
+              digitalWrite(LCDVSS, LOW);
+              digitalWrite(rw, LOW);
+
+              LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+  
+              Serial.begin(9600);// Begin Serial port at a Buad speed of 9600bps 
+              vw_setup(2000);// Setup and Begin communication over the radios at 2000bps( MIN Speed is 1000bps MAX 4000bps)
+              vw_set_rx_pin(RX_PIN);// Set RX Pin 
+              vw_rx_start(); 
+                   
+              }  
+void loop()
+{
+      struct roverRemoteData receivedData;
+      uint8_t rcvdSize = sizeof(receivedData);//Incoming data size 
+      vw_wait_rx();// Start to Receive data now 
+
+if (vw_get_message((uint8_t *)&receivedData, &rcvdSize)) // Check if data is available 
+{
+  if (receivedData.TX_ID == RX_ID) //Check if the radio signal recieved matches the ID of the Reciever
+    { 
+      // If data was Recieved print it to the serial monitor.
+         Serial.println("------------------------New MSG-----------------------");
+         Serial.print("TX ID:");
+         Serial.println(TX_ID);
+         Serial.print("Sensor1Data:");
+         Serial.println(receivedData.Sensor1Data);
+         Serial.print("Sensor2Data:");
+         Serial.println(receivedData.Sensor2Data);
+         Serial.print("SensorData3:");
+         Serial.println(receivedData.Sensor3Data);
+         Serial.print("SensorData4:");
+         Serial.println(receivedData.Sensor4Data);
+        
+         Serial.println("-----------------------End of MSG--------------------");
+        } 
+      else
+        { 
+Serial.println(" ID Does not match waiting for next transmission ");
+        } 
+      } 
+    }
+
---
+++ txCompiled.ino
@@ -0,0 +1,81 @@
+#include <VirtualWire.h>
+#include <VirtualWire_Config.h>
+
+const int AOUTpin=A0;//the AOUT pin of the alcohol sensor goes into analog pin A0 of the arduino
+const int DOUTpin=A1;//the DOUT pin of the alcohol sensor goes into digital pin D8 of the arduino
+const int AGND=A2;
+const int AVCC=A3;
+const int ledPin=13;//the anode of the LED connects to digital pin D13 of the arduino
+const int switchGND=10;
+const int switchPin=9;
+const int switchVCC=8;
+int switchVal;
+int limit;
+int value;
+
+int TX_GND = 5;
+int TX_PIN = 4;// Tell Arduino on which pin you would like to Transmit data NOTE should be a PWM Pin
+int TX_VCC = 2;
+int TX_ID = 3; // Transmitter ID address
+
+struct roverRemoteData// Data Structure 
+{
+int    TX_ID;      // Initialize a storage place for the outgoing TX ID
+int    Sensor1Data;// Initialize a storage place for the first integar that you wish to Send 
+int    Sensor2Data;// Initialize a storage place for the Second integar that you wish to Send
+int    Sensor3Data;// Initialize a storage place for the Third integar that you wish to Send
+int    Sensor4Data;// Initialize a storage place for the Forth integar that you wish to Send
+
+
+};
+
+void setup() {
+
+              pinMode(DOUTpin, INPUT);//sets the pin as an input to the arduino
+              pinMode(AOUTpin, INPUT);
+              pinMode(ledPin, OUTPUT);//sets the pin as an output of the arduino
+              pinMode(switchPin, INPUT);
+              pinMode(AGND, OUTPUT);
+              pinMode(AVCC, OUTPUT);
+              pinMode(switchVCC, OUTPUT);
+              pinMode(switchGND, OUTPUT);
+              pinMode(TX_VCC, OUTPUT);
+              pinMode(TX_GND, OUTPUT);
+              digitalWrite(AGND, LOW);
+              digitalWrite(AVCC, HIGH);
+              digitalWrite(switchGND, LOW);
+              digitalWrite(switchVCC, HIGH);
+              digitalWrite(TX_GND, LOW);
+              digitalWrite(TX_VCC, HIGH);
+
+
+              Serial.begin(9600);// Begin Serial port at a Buad speed of 9600bps 
+              vw_setup(2000);// Setup and Begin communication over the radios at 2000bps( MIN Speed is 1000bps MAX 4000bps)
+              vw_set_tx_pin(TX_PIN);// Set Tx Pin
+ 
+              }
+void loop()
+{   
+
+  
+  struct roverRemoteData payload;// In this section is where you would load the data that needs to be sent.
+
+  
+  // If you want to read a analog pin and transmit its value you can do it as follows by removing the "//" 
+  payload.TX_ID = TX_ID; // Set the Radio Address 
+  payload.Sensor1Data =analogRead(AOUTpin);// analogRead(Sensor1Pin);
+  payload.Sensor2Data =digitalRead(DOUTpin);// analogRead(Sensor2Pin);
+  payload.Sensor3Data =digitalRead(switchPin);// analogRead(Sensor3Pin);
+  payload.Sensor4Data =1;// analogRead(Sensor4Pin);
+ 
+  
+    
+  
+  vw_send((uint8_t *)&payload, sizeof(payload)); // Send the data 
+  vw_wait_tx();// Wait for all data to be sent 
+ // Serial.print(analogRead(AOUTpin));
+  //Serial.print("\n");
+  delay(1000);
+
+}
+
